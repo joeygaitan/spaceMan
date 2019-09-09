cb8b149e8ed0f2e78d647bd2dde445b5850f51dd
Joey Gaitan <joeygaitan707@gmail.com>
2019-09-09 14:37:08 -0700

    added in some comments to my function that tells if you won or not. Finished up another function that displays a string of user guesses that match the secret word. It displays this where there isn't a match -

--- file.py
+++ file.py
@@ -1,15 +1,25 @@
-def is_word_guessed(secret_word, letters_guessed):
-    secretDict = {}
-
+def get_guessed_word(secret_word, letters_guessed):
+# This function checks if the game is over
+    secretDict = {} 
+        # string that will be displayed to the user
+    string = ""
+    # this for loop looks for characters key values  that arent in the dictionary ("a": 0)
     for letter in secret_word:
         if letter not in secretDict:
             secretDict[letter] = 0
+    #This is for counting the guessed letters. It adds them into the key value
         for guessLetter in letters_guessed:
             if letter == guessLetter:
                 print(letter,guessLetter)
                 secretDict[letter] += 1
 
+    #This looks for the zeros so it can fill in the missing letter with a underscore
     for secret_wordletter in secret_word:
         if secretDict[secret_wordletter] == 0:
-            return 
-    return True
+            string += " - "
+        else:
+            string += secret_wordletter
+    return string
+
+
+                
--- game.py
+++ game.py
@@ -10,50 +10,49 @@ def load_word():
     return secret_word
 
 def is_word_guessed(secret_word, letters_guessed):
-    '''
-    A function that checks if all the letters of the secret word have been guessed.
-    Args:
-        secret_word (string): the random word the user is trying to guess.
-        letters_guessed (list of strings): list of letters that have been guessed so far.
-    Returns: 
-        bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
-    '''
-    secretDict = {}
-
+    # this is a dictionary to help check for inputs to  see if you won or not
+    secretDict = {} 
+    # this for loop looks for characters key values  that arent in the dictionary ("a": 0)
     for letter in secret_word:
         if letter not in secretDict:
             secretDict[letter] = 0
+    #This is for counting the guessed letters. It adds them into the key value
         for guessLetter in letters_guessed:
             if letter == guessLetter:
                 print(letter,guessLetter)
                 secretDict[letter] += 1
 
+    # This looks for the and zeros. If it finds a zero then the game is a still on
     for secret_wordletter in secret_word:
         if secretDict[secret_wordletter] == 0:
             return False
     return True
-    # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
 
 def get_guessed_word(secret_word, letters_guessed):
+    # this is a dictionary to help check for inputs to  see if you won or not
+    secretDict = {} 
+    # string that will be displayed to the user
+    string = ""
+    # this for loop looks for characters key values  that arent in the dictionary ("a": 0)
+    for letter in secret_word:
+        if letter not in secretDict:
+            secretDict[letter] = 0
+    #This is for counting the guessed letters. It adds them into the key value
+        for guessLetter in letters_guessed:
+            if letter == guessLetter:
+                print(letter,guessLetter)
+                secretDict[letter] += 1
+    #This looks for the zeros so it can fill in the missing letter with a underscore
+    for secret_wordletter in secret_word:
+        if secretDict[secret_wordletter] == 0:
+            string += "-"
+        else:
+            string += secret_wordletter
+    return string
 
-    '''
-    A function that is used to get a string showing the letters guessed so far in
-    the secret word and underscores for letters that have not been guessed yet.
-    Args: 
-        secret_word (string): the random word the user is trying to guess.
-        letters_guessed (list of strings): list of letters that have been guessed so far.
-    Returns: 
-        string: letters and underscores.  For letters in the word that the user has guessed correctly, 
-        the string should contain the letter at the correct position.  For letters in the word that the user has not yet guessed, 
-        shown an _ (underscore) instead.
-    '''
-
-    #TODO: Loop through the letters in secret word and build a string that shows the letters that have been guessed correctly so far that are saved in letters_guessed and underscores for the letters that have not been guessed yet
-
-    pass
-
-# This function checks if the guess is one of the characters in the sectet word. It will return
-#  a true value if so or a false if not
+# This function checks if the guess is one of the
+# characters in the sectet word. It will return
+# a true value if so or a false if not
 def is_guess_in_word(guess, secret_word):
     for letter in secret_word:
         if guess == letter:
@@ -65,7 +64,8 @@ def is_guess_in_word(guess, secret_word):
 # This function is for displaying the wrongly guessed words
 def wrongGuesses():
     pass
-# This function is for displaying the letters that were guessed right
+# This function is for displaying the 
+# letters that were guessed right
 def rightGuesses():
     pass
 
@@ -73,16 +73,19 @@ def userInputStart(promtps):
     userInput = input(promtps)
     return userInput
 
-# This checks for user guessed input. It filters out other non analphabetical chatacters and only allows one character at a time. 
-def userInput(promtps):
+# This checks for user guessed input. 
+# It filters out other non analphabetical
+# chatacters and only allows one character
+#  at a time.
+def guessInput(promtps):
     userInput = input(promtps)
     if len(userInput) <= 1 or "" != userInput:
         if userInput.isalpha():
             return userInput.upper()
         else:
-            return userInput("Please input only analphabetical character")
+            return guessInput("Please input only analphabetical character")
     else:
-        return userInput("please only input one character")
+        return guessInput("please only input one character")
 
 def selectStart(function_code):
     if function_code == "M" or function_code == "m":
@@ -96,7 +99,6 @@ def selectStart(function_code):
         print("\n Please Type one of the three inputs \n")
         return True
 
-
 def spaceman(secret_word):
     '''
     A function that controls the game of spaceman. Will start spaceman in the command line.
@@ -106,7 +108,7 @@ def spaceman(secret_word):
     runningIntro = True
     runningGame = True
     while runningIntro:
-        selections= userInputStart("Hi Welcome to spaceman. press m if you would like to learn more about the game and rules. Press P if you would like to Play. To Quit please press Q: ")
+        selections= userInputStart("\n Hi Welcome to spaceman. press m if you would like to learn more about\n the game and rules. Press P if you would like to Play. To Quit please press Q: ")
         runningIntro = selectStart(selections)
         if runningIntro == "Quit":
             runningIntro = False
@@ -114,7 +116,8 @@ def spaceman(secret_word):
             print("Thanks for trying out the Program :)")
             return ""
     while runningGame:
-          Selections = userInput()
+          gameSelections = guessInput("")
+          runningGame = gameSelections
           
         # words = is_guess_in_word("stuff",secret_word)
     # return secret_word
